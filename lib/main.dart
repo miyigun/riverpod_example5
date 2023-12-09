d3887b2767dd12d623782882a0ed6e5526bbbeb9
Murat Iyigun <miyigun@hotmail.com>
2023-12-09 18:37:01 +0300

    Commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Riverpod Example 4',
+      title: 'Riverpod Example 5',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Riverpod Example 4'),
+      home: const MyHomePage(title: 'Riverpod Example 5'),
     );
   }
 }
